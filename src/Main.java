098ea9c93c8ce199b461fd406c6d9dd22fbd3805
valerakaj <valerakajdalova@gmail.com>
2018-05-18 16:41:44 +0300

    types of variables, values adding functions

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        hello("world");
+
+
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + "-" + area(l));
+
+        double a = 4;
+        double b = 6;
+
+
+        System.out.println("Площадь прямоугольника со сторонами " + a + "и " + b + " = " + area(a, b));
+
+        }
+
+            private static void hello(String somebody) {
+            System.out.println("Hello, " + somebody + "!");
+
+            }
+
+            private static double area(double len) {
+                return len * len;
+
+            }
+
+            private static double area(double a, double b) {
+                return a * b;
+
+            }
+
+
     }
-}
